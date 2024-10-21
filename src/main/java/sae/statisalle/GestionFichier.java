1c66134c422e7b16f11fc2c9bac09a92a4938675
erwan.thierry <erwan.thierry@iut-rodez.fr>
2024-10-21 14:38:52 +0200

    Création de la classe GestionFichier

---
+++ src/main/java/sae/statisalle/GestionFichier.java
@@ -0,0 +1,63 @@
+package sae.statisalle;
+
+import java.io.File;
+
+/*
+ *
+ */
+public class GestionFichier {
+
+    /*
+     * Elle permet de récupérer 1 seul fichier CSV en réseau local
+     * @return
+     * @throws
+     */
+    public void importerFichierCsv() {
+        // TODO: Implémenter le code pour importer un fichier CSV
+    }
+
+    // Méthode pour exporter un fichier CSV
+    public void exporterFichierCsv() {
+        // TODO: Implémenter le code pour exporter un fichier CSV
+    }
+
+    // Méthode pour exporter des fichiers réseaux
+    public void exporterFichierReseaux() {
+        // TODO: Implémenter le code pour exporter des fichiers réseaux
+    }
+
+    // Méthode pour importer des fichiers réseaux
+    public void importerFichierReseaux() {
+        // TODO: Implémenter le code pour importer des fichiers réseaux
+    }
+
+    // Méthode pour générer un fichier PDF
+    public File generationPDF(String chemin) {
+        // TODO: Implémenter le code pour générer un fichier PDF
+        return new File(chemin); // BOUCHON
+    }
+
+    // Méthode pour écrire dans un fichier
+    public File ecritureFichier(String chemin) {
+        // TODO: Implémenter le code pour écrire dans un fichier
+        return new File(chemin); // BOUCHON
+    }
+
+    // Méthode pour lire un fichier
+    public File lectureFichier(String chemin) {
+        // TODO: Implémenter le code pour lire un fichier
+        return new File(chemin); // BOUCHON
+    }
+
+    // Méthode pour sauvegarder un fichier
+    public File sauvegardeFichier(String chemin) {
+        // TODO: Implémenter le code pour sauvegarder un fichier
+        return new File(chemin); // BOUCHON
+    }
+
+    // Méthode pour charger un fichier
+    public File chargerFichier(String chemin) {
+        // TODO: Implémenter le code pour charger un fichier
+        return new File(chemin); // BOUCHON
+    }
+}
