854a1021ce5a783a2ef5918daf6aa17df463d868
erwan.thierry <erwan.thierry@iut-rodez.fr>
2024-10-21 15:32:57 +0200

    Début de GestionFichier, création d'un objet GestionFichier afin de simplifier la manipulation

--- src/main/java/sae/statisalle/test/TestGestionFichier.java
+++ src/main/java/sae/statisalle/test/TestGestionFichier.java
@@ -2,5 +2,15 @@ package sae.statisalle.test;
 
 public class TestGestionFichier {
 
+    /**
+     * Test des 3 méthodes de parcours
+     */
 
+    public static void testImportation() {
+        System.out.println("\nTest de la méthode Importation :\n"
+                + "-------------------------------------\n");
+
+        System.out.println("Importation de fichier avec l'extension .csv ou bien .CSV :");
+
+    }
 }
