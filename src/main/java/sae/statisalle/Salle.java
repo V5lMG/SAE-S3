6c9623e847f4bad916b2c0702baf583daf5bcb6f
robin <robin.montes@iut-rodez.fr>
2024-10-23 14:31:14 +0200

    Création du constructeur avec les parametres et les variables globales et ajout de Javadoc

--- src/main/java/sae/statisalle/Salle.java
+++ src/main/java/sae/statisalle/Salle.java
@@ -3,6 +3,7 @@
  * IUT DE RODEZ               Pas de copyrights
  */
 package sae.statisalle;
+import java.util.List;
 
 /**
  * La classe Salle initialise les objets de type Salle.
@@ -13,8 +14,37 @@ package sae.statisalle;
  * @author robin.montes
  */
 public class Salle {
+    /*Identifiant de la salle*/
+    String identifiant;
 
-    public Salle(){
+    /*Capacité de place de la salle*/
+    int capacite;
 
+    /*Indique la présence de vidéo projecteur dans la salle*/
+    boolean videoProj;
+
+    /*Indique la présence d'écran XXL dans la salle*/
+    boolean ecranXXL;
+
+    /*Nombre de machine présente dans la salle*/
+    int nbMachine;
+
+    /*Description de la salle */
+    String description;
+
+    /*Liste des logiciels utilisable dans la salle*/
+    List<String> logiciel;
+
+    /*Indique la présence d'imprimante dans la salle*/
+    boolean imprimante;
+
+    public Salle(String indentifiant, int capacite, boolean videoProj, boolean ecranXXL, int nbMachine, String description, boolean imprimante) {
+        this.identifiant = indentifiant;
+        this.capacite = capacite;
+        this.videoProj = videoProj;
+        this.ecranXXL = ecranXXL;
+        this.nbMachine = nbMachine;
+        this.description = description;
+        this.imprimante = imprimante;
     }
 }
