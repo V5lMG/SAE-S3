9301ef7586ddaf6091a131d058634465c6181061
robin <robin.montes@iut-rodez.fr>
2024-10-23 14:14:54 +0200

    Création du constructeur et ajout de Javadoc

--- src/main/java/sae/statisalle/Salle.java
+++ src/main/java/sae/statisalle/Salle.java
@@ -1,4 +1,20 @@
+/*
+ * Salle.java               23/10/2024
+ * IUT DE RODEZ               Pas de copyrights
+ */
 package sae.statisalle;
 
+/**
+ * La classe Salle initialise les objets de type Salle.
+ * Elle permet également de vérifier si une salle existe déjà
+ * <br>
+ * Elle gère également toutes les erreurs relatives à l'instantiation
+ * des salles en fonction des contenues des fichier
+ * @author robin.montes
+ */
 public class Salle {
+
+    public Salle(){
+
+    }
 }
