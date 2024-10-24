40c03c2e8cbca55c8155690f571aac7fa0871281
robin <robin.montes@iut-rodez.fr>
2024-10-24 19:09:36 +0200

    Création des contructeurs et javadoc des classes Activité, Employe, Creneau, Reservation et correction de la doc sur Salle

--- src/main/java/sae/statisalle/Employe.java
+++ src/main/java/sae/statisalle/Employe.java
@@ -1,4 +1,36 @@
+/*
+ * Employe.java               23/10/2024
+ * IUT DE RODEZ               Pas de copyrights
+ */
 package sae.statisalle;
 
+/**
+ * La classe Employe initialise les objets de type Employe.
+ * L'objet Employe est composé du :
+ * <ul>
+ *     <li>Nom</li>
+ *     <li>Prenom</li>
+ *     <li>Numéros de téléphone</li>
+ *     <li>email</li>
+ * </ul>
+ * <br>
+ * Elle gère également toutes les erreurs relatives à l'instantiation
+ * des Employés en fonction des contenus des fichiers
+ * @author robin.montes
+ */
 public class Employe {
+    /*Nom de l'employé*/
+    String nom;
+
+    /*Prenom de l'employé*/
+    String prenom;
+
+    /*Numéro de téléphone de l'employé*/
+    String numTel;
+
+    public Employe(String nom, String prenom, String numTel) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.numTel = numTel;
+    }
 }
--- src/main/java/sae/statisalle/Reservation.java
+++ src/main/java/sae/statisalle/Reservation.java
@@ -1,5 +1,38 @@
+/*
+ * Reservation.java               24/10/2024
+ * IUT DE RODEZ               Pas de copyrights
+ */
 package sae.statisalle;
 
-public class Reservation {
+import java.text.DateFormat;
 
+/**
+ * La classe Reservation initialise les objets de type Reservation.
+ * L'objet Reservation est composé de :
+ * <ul>
+ *     <li>Un identifiant de réservation</li>
+ *     <li>Une date</li>
+ *     <li>Un Creneau(Cf classe Creneau)</li>
+ * </ul>
+ *
+ * <br>
+ * Elle gère également toutes les erreurs relatives à l'instantiation
+ * des reservations en fonction des contenus des fichier
+ * @author robin.montes
+ */
+public class Reservation {
+    /*Identifiant de la reservation*/
+    String idReservation;
+
+    /*Date de la réservation*/
+    DateFormat date;
+
+    /*Créneau de réservation*/
+    Creneau creneau;
+
+    public Reservation(String idReservation, DateFormat date, Creneau creneau) {
+        this.idReservation = idReservation;
+        this.date = date;
+        this.creneau = creneau;
+    }
 }
