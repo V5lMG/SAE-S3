6f394ea5ae062b01033be61ee2f589b2d2c52bb0
robin <robin.montes@iut-rodez.fr>
2024-10-24 19:09:36 +0200

    Création des contructeurs et javadoc des classes Activité, Employe, Creneau, Reservation et correction de la doc sur Salle

--- src/main/java/sae/statisalle/Activite.java
+++ src/main/java/sae/statisalle/Activite.java
@@ -1,4 +1,25 @@
+/*
+ * Activite.java               24/10/2024
+ * IUT DE RODEZ               Pas de copyrights
+ */
 package sae.statisalle;
 
+/**
+ * La classe Activite initialise les objets de type Activite.
+ * L'objet Activite est composé de :
+ * <ul>
+ *     <li>Un type d'activité</li>
+ * </ul>
+ * <br>
+ * Elle gère également toutes les erreurs relatives à l'instantiation
+ * des Activités en fonction des contenus des fichiers
+ * @author robin.montes
+ */
 public class Activite {
+    /*Les différents types d'activité*/
+    String type;
+
+    public Activite(String type) {
+        this.type = type;
+    }
 }
--- src/main/java/sae/statisalle/Salle.java
+++ src/main/java/sae/statisalle/Salle.java
@@ -3,14 +3,24 @@
  * IUT DE RODEZ               Pas de copyrights
  */
 package sae.statisalle;
-import java.util.List;
+import java.util.ArrayList;
 
 /**
  * La classe Salle initialise les objets de type Salle.
- * Elle permet également de vérifier si une salle existe déjà
+ * L'objet Salle est constitué de :
+ * <ul>
+ *     <li>Un identifiant</li>
+ *     <li>Une capacité</li>
+ *     <li>Un indicateur de la présence d'un video projecteur</li>
+ *     <li>Un indicateur de la présence d'un écran XXL</li>
+ *     <li>Le nombre de machine</li>
+ *     <li>Une description</li>
+ *     <li>La liste des logiciels</li>
+ *     <li>Un indicateur de la présence d'une imprimante</li>
+ * </ul>
  * <br>
  * Elle gère également toutes les erreurs relatives à l'instantiation
- * des salles en fonction des contenues des fichier
+ * des salles en fonction des contenus des fichier
  * @author robin.montes
  */
 public class Salle {
@@ -33,18 +43,18 @@ public class Salle {
     String description;
 
     /*Liste des logiciels utilisable dans la salle*/
-    List<String> logiciel;
+    ArrayList<String> logiciel;
 
     /*Indique la présence d'imprimante dans la salle*/
     boolean imprimante;
 
-    public Salle(String indentifiant, int capacite, boolean videoProj, boolean ecranXXL, int nbMachine, String description, boolean imprimante) {
+    public Salle(String indentifiant, int capacite, boolean videoProj, boolean ecranXXL, int nbMachine, ArrayList<String> logiciel, boolean imprimante) {
         this.identifiant = indentifiant;
         this.capacite = capacite;
         this.videoProj = videoProj;
         this.ecranXXL = ecranXXL;
         this.nbMachine = nbMachine;
-        this.description = description;
+        this.logiciel = new ArrayList<String>();
         this.imprimante = imprimante;
     }
 }
