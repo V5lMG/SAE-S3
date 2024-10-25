d1e223e6de5bd3d62c72eb74c23e76227a2d6309
tabordamadalenojoao <rodrigo.xaviertaborda@iut-rodez.fr>
2024-10-25 08:40:44 +0200

    Création de la classe TestReseau

---
+++ src/main/java/sae/statisalle/test/TestReseau.java
@@ -0,0 +1,12 @@
+package sae.statisalle.test;
+import sae.statisalle.Reseau;
+
+public class TestReseau {
+
+    public static void main(String[] args){
+        envoyerDonnees();
+    }
+
+    private static void envoyerDonnees() {
+    }
+}
